99f3743043a7a4ac5bdaa3828721545050a16d51
Narom Kuoch <narom.kuoch@gmail.com>
2021-04-30 11:36:37 -0500

    version catlog kts failure

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-   
     repositories {
         google()
         jcenter()
     }
-//    print("${libs.versions.kotlin}")
+
     dependencies {
-        classpath libs.agp
-        classpath libs.kotlin
+        classpath(libs.agp)
+        classpath(libs.kotlin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -20,8 +19,4 @@ allprojects {
         google()
         jcenter()
     }
-}
-
-task clean(type: Delete) {
-    delete rootProject.buildDir
 }
