2f144ba6406dc7b7955985ce1cd64dacc6c2651e
Narom Kuoch <narom.kuoch@gmail.com>
2021-04-30 11:37:30 -0500

    version catlog with kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,23 @@
-include ':app'
+
+
+fun includeProject(name: String, filePath: String? = null){
+    settings.include(name)
+    val project = project(name)
+    if(filePath != null) {
+        project.projectDir = File(rootDir, filePath)
+    }
+    project.configureBuildFileName(name)
+}
+
+fun ProjectDescriptor.configureBuildFileName(projectName: String) {
+    val name = projectName.substringAfterLast(":")
+    buildFileName = "$name.gradle"
+    if(!buildFile.exists()){
+        buildFileName = "$name.gradle.kts"
+    }
+}
+
+includeProject( ":app")
 rootProject.name = "Experiment"
+
+enableFeaturePreview("VERSION_CATALOGS")
